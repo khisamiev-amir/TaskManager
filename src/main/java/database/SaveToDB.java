67f3f553d480e1b69dd9f932255354634b90836e
AKhisamiev <khisamiev_amir@mail.ru>
2023-07-18 10:25:01 +0300

    add commit

---
+++ src/main/java/database/SaveToDB.java
@@ -0,0 +1,11 @@
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Slf4j
+public class SaveToDB {
+//    public static void DBActions(String s){
+//        log.trace(s);
+//
+//    }
+}
